ad31afa2125d8f3bf7498dc0c406cfc17b16354e
bakrolee <104603760+bakrolee@users.noreply.github.com>
2022-05-18 16:32:55 +0900

    Add files via upload

---
+++ 22.05.17/PrintName.java
@@ -0,0 +1,17 @@
+public class PrintName {
+    public static void main(String[] args) {
+        System.out.println("이박로1");
+        System.out.println("이박로2");
+        System.out.println("이박로3");
+        System.out.println("이박로4");
+        System.out.println("이박로5");
+
+        System.out.println("10" + "10");
+        System.out.println("11" + "11");
+        System.out.println("12" + "12");
+
+        System.out.println(20 + 20);
+        System.out.println(21 + 21);
+        System.out.println(22 + 22);
+    }
+}

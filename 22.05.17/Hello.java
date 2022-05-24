88dc1ff44556f42b54e630c2e4481f7ded8a2993
bakrolee <104603760+bakrolee@users.noreply.github.com>
2022-05-24 09:08:00 +0900

    Delete 22.05.17 directory

--- 22.05.17/Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {  // 메인 메소드
-	// 문자열 (String)
-        System.out.println("Hello World"); // 문장
-	System.out.println("12345");
-	System.out.println("010-5039-5692");
-    }
-}
-

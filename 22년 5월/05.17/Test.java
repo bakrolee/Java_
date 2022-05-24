1a84396bb04484a5a87b6f8c7c45ccda5c3d9f2e
bakrolee <104603760+bakrolee@users.noreply.github.com>
2022-05-24 09:05:28 +0900

    Add files via upload

---
+++ 22년 5월/05.17/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+	int x;
+	double y; // double은 실수형 변수
+
+	x = 44;
+	y = 72;
+
+	System.out.println(x + y);
+	System.out.println(x - y);
+	System.out.println(x * y);
+	System.out.println(x / y);
+    }
+}

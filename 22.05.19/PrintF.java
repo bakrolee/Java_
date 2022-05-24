04efb05ad0dedfcf02aae4c22a2cd067d2a76973
bakrolee <104603760+bakrolee@users.noreply.github.com>
2022-05-24 09:07:44 +0900

    Delete 22.05.19 directory

--- 22.05.19/PrintF.java
+++
@@ -1,8 +0,0 @@
-public class PrintF {
-	public static void main(String[] args) {
-		System.out.printf("%d Hello %d World %d\n", 4, 1, 9);
-		System.out.printf("Hello World" );
-		System.out.print(1.0%3.0 );
-	}
-}
-
